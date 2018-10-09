2e9ba0b5cf3d31e9fd66710a8239ece35633104b
Taishawn Blackmon <taishawnblackmon@gmail.com>
2018-10-08 21:38:21 -0400

    lab part 2

--- components/to-do-app/todoapp-controller.js
+++ components/to-do-app/todoapp-controller.js
@@ -4,10 +4,26 @@
      .controller('ToDoController', function(){
          const $ctrl = this;
          $ctrl.shop = [
-             {list: 'Walk The Dog', done: false},
-            {list: 'Go to the store', done: true},
+            {list: 'Walk The Dog', done: false},
+            {list: 'Go to the store', done: false},
             {list:'Get gas', done: false},
             {list: 'Wash the car', done: false},
             ];
+            
+        $ctrl.addTask = function() {
+            $ctrl.shop.push({list: $ctrl.newTask, done: false});
+            $ctrl.newTask = '';
+        }
+ 
+        $ctrl.completeTask = (index) => {
+            $ctrl.shop[index].done = true;
+        }
+
+        $ctrl.removeTask = function(index) {
+            $ctrl.shop.splice(index,1)  
+        }
+        
+
+        
      })
 }
